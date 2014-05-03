6aeedd4def01b5be0e4c00c45549855426409899
Josh Moles <josh.moles@gmail.com>
2014-05-03 13:08:49 -0700

    Removing example file.

--- example.py
+++
@@ -1,6 +0,0 @@
-QPropertyAnimation *animation = new QPropertyAnimation(myWidget, "geometry");
-animation->setDuration(10000);
-animation->setStartValue(QRect(0, 0, 100, 30));
-animation->setEndValue(QRect(250, 250, 100, 30));
-
-animation->start();
